ca43dec1aa1d163575e7116d7d7d1c4ba354c469
Unknown <taruerick@gmail.com>
2017-05-03 17:15:05 +0200

    logo

--- view/includes/navbar.php
+++ view/includes/navbar.php
@@ -3,11 +3,12 @@
   <li><a href="#!">Connexion</a></li>
   <li><a href="../controller/controller_form.php">New request</a></li>
   <li class="divider"></li>
-  <li><a href="#!">three</a></li>
 </ul>
 <nav class="light-blue">
   <div class="nav-wrapper">
-    <a href="../controller/controller_request.php" class="brand-logo">Logo</a>
+	<ul>
+		<a href="../controller/controller_request.php" class="brand-logo"> <img class="responsive-img" src="../images/logo2.png" height="121" width="121"> </a>
+	</ul>  
     <ul class="right hide-on-med-and-down">
       <li><a href="../controller/controller_members.php">Members</a></li>
       <li><a href="../controller/controller_list_request.php">Request</a></li>
